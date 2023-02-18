66fd2831d8eadac5a422fd29afcd6d8c414afbe1
CRS <crsc2222@outlook.com>
2023-02-18 15:09:38 -0600

    lexical diversity added

--- Hw2/Hw2.py
+++ Hw2/Hw2.py
@@ -1,25 +1,45 @@
+#Homework 2
+#Cris Chou
+
 import nltk
-nltk.download()
+#nltk.download()
 from nltk import word_tokenize
 from nltk import sent_tokenize
 import sys
-
+import re
+import pathlib
+import os
 
 #read file
 
 
 #preprocess file
-def preprocess(file):
-    print("Hello")
+#def preprocess(text):
+  #  text = text.lower()
+    
 
 
     
-if __name__ == 'main':
-    if(sys.argv) > 2:
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
         print("Please use valid file path: ")
         quit()
 
-        fp = sys.argv[1]
-        with open(pathlib.Path.cwd() / fp) as f:
-            
-            text_in = f.read()
+    fp = sys.argv[1]
+    with open(pathlib.Path.cwd().joinpath(fp), 'r') as f:
+        #tokenize file
+        text_in = f.read()
+        tokens = word_tokenize(text_in)
+
+        #calculating lexical diversity
+        #get total words
+        total_words = len(tokens)
+    
+        #get unique words
+        unique_words = len(set(tokens))
+
+        #get lexical diversity
+        lexical_diversity = unique_words/total_words
+        print("Lexical Diversity: ", str(round(lexical_diversity,2)))
+
+        #preprocess(text_in)
