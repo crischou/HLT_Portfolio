fb8c489b9fa2ba1ef853fe59f16ee8afabb40ebd
CRS <crsc2222@outlook.com>
2023-02-18 13:37:38 -0600

    Updated main file

--- Hw2/Hw2.py
+++ Hw2/Hw2.py
@@ -1,4 +1,5 @@
 import nltk
+nltk.download()
 from nltk import word_tokenize
 from nltk import sent_tokenize
 import sys
@@ -9,3 +10,16 @@ import sys
 
 #preprocess file
 def preprocess(file):
+    print("Hello")
+
+
+    
+if __name__ == 'main':
+    if(sys.argv) > 2:
+        print("Please use valid file path: ")
+        quit()
+
+        fp = sys.argv[1]
+        with open(pathlib.Path.cwd() / fp) as f:
+            
+            text_in = f.read()
