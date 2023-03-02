3033e4b75910d73e8f3af3549a06f7702251fe66
CRS <crsc2222@outlook.com>
2023-03-02 17:01:03 -0600

    started langMod

--- Hw4/Hw4.py
+++ Hw4/Hw4.py
@@ -3,3 +3,19 @@
 
 
 import nltk
+from nltk import word_tokenize
+from nltk import bigrams
+from nltk import unigrams
+
+#create a function witha filename as argument
+
+def langMod(text_in):
+    #read text and remove newlines
+    text = open(text_in).read().replace('\n','')
+    #tokenize text
+    tokens = nltk.word_tokenize(text)
+    #use nltk to create bigrams list
+    bigrams = nltk.bigrams(tokens)
+    #use nltk to create unigrams list
+    unigrams = nltk.unigrams(tokens)
+    
