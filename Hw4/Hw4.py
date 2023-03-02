7b37a077b96e2afd59ba1c9c0e0d8e1d5cf13c5b
CRS <crsc2222@outlook.com>
2023-03-02 17:55:28 -0600

    finished langMod starting main

--- Hw4/Hw4.py
+++ Hw4/Hw4.py
@@ -1,6 +1,8 @@
 #Cris Chou
 #Homework 4
 
+import sys
+import pathlib
 
 import nltk
 from nltk import word_tokenize
@@ -21,3 +23,30 @@ def langMod(text_in):
 
     #use the bigram list to create bigram dict with counts
     bigram_dict = {}
+    for bigram in bigrams:
+        if bigram in bigram_dict:
+            bigram_dict[bigram] += 1
+        else:
+            bigram_dict[bigram] = 1
+
+    #do same thing with unigram
+    unigram_dict = {}
+    for unigram in unigrams:
+        if unigram in unigram_dict:
+            unigram_dict[unigram] += 1
+        else:
+            unigram_dict[unigram] = 1
+    
+    #return bigram and unigram dicts
+    return bigram_dict, unigram_dict
+
+ 
+
+if __name__ == '__main__':
+    
+    #function call on training files
+
+    bidictEn, unidictEn = langMod('/data/LangId.train.English')
+    bidictFr, unidictFr = langMod('/data/LangId.train.French')
+    bidictIt, unidictIt = langMod('/data/LangId.train.Italian')
+    
