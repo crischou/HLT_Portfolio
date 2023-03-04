97686b9a9ab21b5ce1c22d6aa7c5eec2abe46a08
CRS <crsc2222@outlook.com>
2023-03-04 17:45:05 -0600

    updated output

--- Hw4/Output.py
+++ Hw4/Output.py
@@ -15,35 +15,51 @@ unidictFr = pickle.load(open('unidictFr.p', 'rb'))
 bidictIt = pickle.load(open('bidictIt.p', 'rb'))
 unidictIt = pickle.load(open('unidictIt.p', 'rb'))
 
-#open test file
-test = open('data\LangId.test')
 
 #probablility of each language
-def compute_prob(text_in, unigram_dict, bigram_dict, N, V):
-    #N = number of tokens in training data
-    #V = unique tokens in training data
-
+def compute_prob(text_in, unigram_dict, bigram_dict, V):
+    #V = vocab size
     unigrams_test = word_tokenize(text_in)
     bigrams_test = list(ngrams(unigrams_test,2))
-
+    
     #p of using a variation of laplace smoothing
-
-    p_gt = 1
     p_laplace = 1 
 
     for bigram in bigrams_test:
         n = bigram_dict[bigram] if bigram in bigram_dict else 0
-        n_gt = bigram_dict[bigram] if bigram in bigram_dict else 1/N
         d = unigram_dict[bigram[0]] if bigram[0] in unigram_dict else 0
-        if d == 0:
-            p_gt *= 1/N
-        else:
-            p_gt *= n_gt/d
-            
+        #print(n, "", d)
+    
         p_laplace *= ((n+1)/(d+V))
-        
-    print("Probability with laplace smoothing is %.5f" % (p_gt))
+    
+    #print(p_laplace)
+    #print("Probability with laplace smoothing is %.5f" % p_laplace)
 
+    return p_laplace
 
     
-    
+if __name__ == '__main__':
+
+    #get vocab size
+    vocab_size = len(unidictEn) + len(unidictFr) + len(unidictIt)
+    print(vocab_size)
+
+    with open('data\LangId.test') as f:
+            lines = f.readlines()
+
+            for line in lines:
+                en_prob = compute_prob(line, unidictEn, bidictEn, vocab_size)
+                fr_prob = compute_prob(line, unidictFr, bidictFr, vocab_size)
+                it_prob = compute_prob(line, unidictIt, bidictIt, vocab_size)
+                
+                if en_prob > fr_prob and en_prob > it_prob:
+                    print("English")
+                elif fr_prob > en_prob and fr_prob > it_prob:
+                    print("French")
+                elif it_prob > en_prob and it_prob > fr_prob:
+                    print("Italian")
+
+    #function call
+    #compute_prob(lines, unidictEn, bidictEn, vocab_size)
+    #compute_prob(lines, unidictFr, bidictFr, vocab_size)
+    #compute_prob(lines, unidictIt, bidictIt, vocab_size)
