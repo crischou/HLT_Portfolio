cd1d5aafd38c0649ec6c1a60124cf4261c0f98de
CRS <crsc2222@outlook.com>
2023-03-04 15:23:33 -0600

    updated main

--- Hw4/Main.py
+++ Hw4/Main.py
@@ -6,8 +6,7 @@ import pathlib
 
 import nltk
 from nltk import word_tokenize
-from nltk import bigrams
-from nltk import unigrams
+from nltk.util import ngrams
 
 import pickle
 
@@ -15,13 +14,13 @@ import pickle
 
 def langMod(text_in):
     #read text and remove newlines
-    text = open(text_in).read().replace('\n','')
+    tokens = open(text_in).read().replace('\n','')
     #tokenize text
-    tokens = nltk.word_tokenize(text)
+    unigrams = nltk.word_tokenize(text_in)
     #use nltk to create bigrams list
-    bigrams = nltk.bigrams(tokens)
+    bigrams = list(ngrams(tokens,2))
     #use nltk to create unigrams list
-    unigrams = nltk.unigrams(tokens)
+    #unigrams = nltk.unigrams(tokens)
 
     #use the bigram list to create bigram dict with counts
     bigram_dict = {}
@@ -33,16 +32,16 @@ def langMod(text_in):
     
     #return bigram and unigram dicts
     return bigram_dict, unigram_dict
-
- 
+    
+    
 
 if __name__ == '__main__':
     
     #function call on training files
 
-    bidictEn, unidictEn = langMod('/data/LangId.train.English')
-    bidictFr, unidictFr = langMod('/data/LangId.train.French')
-    bidictIt, unidictIt = langMod('/data/LangId.train.Italian')
+    bidictEn, unidictEn = langMod('data\LangId.train.English')
+    bidictFr, unidictFr = langMod('data\LangId.train.French')
+    bidictIt, unidictIt = langMod('data\LangId.train.Italian')
 
     #pickle dicts
     pickle.dump(bidictEn, open('bidictEn.p', 'wb'))
@@ -51,5 +50,7 @@ if __name__ == '__main__':
     pickle.dump(unidictFr, open('unidictFr.p', 'wb'))
     pickle.dump(bidictIt, open('bidictIt.p', 'wb'))
     pickle.dump(unidictIt, open('unidictIt.p', 'wb'))
+
+
 
     
