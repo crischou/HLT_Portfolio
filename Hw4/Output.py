db6d7d0addcbafc89f8b769004fba20d6391cbad
CRS <crsc2222@outlook.com>
2023-03-04 18:25:37 -0600

    updated output

--- Hw4/Output.py
+++ Hw4/Output.py
@@ -22,19 +22,22 @@ def compute_prob(text_in, unigram_dict, bigram_dict, V):
     unigrams_test = word_tokenize(text_in)
     bigrams_test = list(ngrams(unigrams_test,2))
     
+    #print(bigrams_test)
+    #print(unigram_dict)
     #p of using a variation of laplace smoothing
     p_laplace = 1 
 
     for bigram in bigrams_test:
+
+        
         n = bigram_dict[bigram] if bigram in bigram_dict else 0
         d = unigram_dict[bigram[0]] if bigram[0] in unigram_dict else 0
-        #print(n, "", d)
-    
+
         p_laplace *= ((n+1)/(d+V))
     
     #print(p_laplace)
     #print("Probability with laplace smoothing is %.5f" % p_laplace)
-
+    
     return p_laplace
 
     
@@ -44,6 +47,9 @@ if __name__ == '__main__':
     vocab_size = len(unidictEn) + len(unidictFr) + len(unidictIt)
     print(vocab_size)
 
+
+    check = []
+    count = 0
     with open('data\LangId.test') as f:
             lines = f.readlines()
 
@@ -51,15 +57,29 @@ if __name__ == '__main__':
                 en_prob = compute_prob(line, unidictEn, bidictEn, vocab_size)
                 fr_prob = compute_prob(line, unidictFr, bidictFr, vocab_size)
                 it_prob = compute_prob(line, unidictIt, bidictIt, vocab_size)
-                
-                if en_prob > fr_prob and en_prob > it_prob:
-                    print("English")
-                elif fr_prob > en_prob and fr_prob > it_prob:
-                    print("French")
-                elif it_prob > en_prob and it_prob > fr_prob:
-                    print("Italian")
 
-    #function call
-    #compute_prob(lines, unidictEn, bidictEn, vocab_size)
-    #compute_prob(lines, unidictFr, bidictFr, vocab_size)
-    #compute_prob(lines, unidictIt, bidictIt, vocab_size)
+                #find out which one is most likely
+                if en_prob > fr_prob and en_prob > it_prob:
+                    check.append("English")
+                elif fr_prob > en_prob and fr_prob > it_prob:
+                    check.append("French")
+                elif it_prob > en_prob and it_prob > fr_prob:
+                    check.append("Italian")
+                else:
+                    check.append("Unknown")
+                
+    total = len(check)
+    correct = 0
+    #check for accuracy
+    with open('data\LangId.sol') as f:
+
+        lines = f.readlines()
+        for i in range(len(lines)):
+            #remove the numbers to compare
+            if lines[i].strip()[2:] == check[i]:
+                correct += 1
+            print(lines[i].strip()[2:], " ", check[i])
+            if i == 50:
+                break
+    print("Accuracy is %.2f" % (correct/total))
+    
