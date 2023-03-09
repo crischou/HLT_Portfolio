8da1dc5ad43a0699abb75faf4868a179eb506120
CRS <crsc2222@outlook.com>
2023-03-08 22:04:50 -0600

    started hw5

--- Hw5/Hw5.py
+++ Hw5/Hw5.py
@@ -1,3 +1,12 @@
 #Cris Chou
 #Homework 5
 
+
+
+from bs4 import BeautifulSoup
+import requests
+import urllib as ul
+import wikipedia
+
+#choose a website with 15 external urls
+url = 'https://en.wikipedia.org/wiki/Three-body_problem'
