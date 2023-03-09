3ce478e0d743f0f398f86ec2143e4c71d945a4bb
CRS <crsc2222@outlook.com>
2023-03-08 22:18:08 -0600

    started parsing website

--- Hw5/Hw5.py
+++ Hw5/Hw5.py
@@ -10,3 +10,17 @@ import wikipedia
 
 #choose a website with 15 external urls
 url = 'https://en.wikipedia.org/wiki/Three-body_problem'
+
+
+def webCrawl(url): 
+    #loop through urls
+    for i in range(15):
+        res = ul.request.urlopen(url)
+        text = res.read().decode('UTF-8')
+        soup = BeautifulSoup(text, 'html5lib')
+        #scrape text from each url and put into a file
+        with open('webCrawl.txt', 'a') as f:
+            f.write(soup.get_text())
+            
+
+webCrawl(url)
