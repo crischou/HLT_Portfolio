b35009f138f53a5a87ce1bea7110f0aa29faa5a4
CRS <crischou@users.noreply.github.com>
2023-04-14 16:07:37 -0500

    get user year

--- chatbot/main.py
+++ chatbot/main.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import random
 from nltk.sentiment import SentimentIntensityAnalyzer
-
+import re
 
 class Chatbot:
 
@@ -17,10 +17,14 @@ class Chatbot:
         self.lemmatizer = WordNetLemmatizer()
 
     def chat(self):
+
+        #pattern for grade/year
+        grade_pattern = r'freshmen|sophmore|junior|senior'
+
         name = input("Hello, what is your name? ")
         #check if new user or returning user
         if name not in self.users:
-            self.users[name] = {}
+            self.users[name] = {'name': name, 'grade': [], 'messages': [], 'responses': [], 'sentiment': []}
             self.greet(name)
 
         elif name in self.users:
@@ -35,17 +39,35 @@ class Chatbot:
                 self.goodbye(name)
                 break
             else:
-                response = self.generate_response(name,message)
-                print("Chatbot: " + response)
+                #check if user states their grade/year
+                match = re.search(grade_pattern,message,re.IGNORECASE)
+                if match:
+                    grade = self.grade(name,match.group(0))
+
+                if("grade" in message.lower() or "year" in message.lower()):
+                    if len(self.users[name]['grade']) == 0:
+                        print("You haven't told me your grade yet.")
+                    else:
+                        print("You are a " + self.users[name]['grade'][0] + " at UTD.")
+               
+                else:    
+                    response = self.generate_response(name,message)
+                    print("Chatbot: " + response)
+                
 
 
+    #greetings
     def greet(self, name):
         greetings = ["Hello","Hi","Hey","Howdy", "What's up","How are you doing"]
         print(random.choice(greetings) + " " + name + "!")
-
+    #goodbyes
     def goodbye(self, name):
         goodbyes = ["Goodbye","Bye","See you later","See you next time", "Have a good day"]
         print(random.choice(goodbyes) + " " + name)
+
+    #keep track of user grades
+    def grade(self, name,grade):
+        self.users[name]['grade'].append(grade)
 
     def generate_response(self, name, message):
         #sentiment analysis
@@ -62,4 +84,10 @@ class Chatbot:
 
 
 chatbot = Chatbot()
-chatbot.chat()
+
+cont = True
+while cont:
+    chatbot.chat()
+    ans = input("Would you like to chat again? (y/n): ")
+    if ans == "n":
+        cont = False
